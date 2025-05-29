9804d0c5a9685ca434ecc8afe590ead21a1d882f
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-05-28 23:46:10 -0400

    Make the menu use windows

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -3,10 +3,21 @@ extends Control
 signal create_server
 signal join_server(ip: String)
 
+@onready var main_menu: Window = $MainMenu
+@onready var join_server_menu: Window = $JoinServerMenu
+@onready var ip_address_join_edit: LineEdit = $JoinServerMenu/PanelContainer/MarginContainer/MarginContainer/GridContainer/IPAddressJoinEdit
+
 
 func _on_create_server_button_pressed() -> void:
 	create_server.emit()
+	main_menu.hide()
 
 
 func _on_join_server_button_pressed() -> void:
-	join_server.emit("127.0.0.1")
+	join_server_menu.show()
+
+
+func _on_join_server_window_join_button_pressed() -> void:
+	join_server.emit(ip_address_join_edit.text)
+	main_menu.hide()
+	join_server_menu.hide()
